ee91e40f2def28e45eddc940d3964e55b3cfb084
Anuj_Saini <anuj2196@gmail.com>
2018-11-17 17:38:12 +0530

    Create Student and Teacher model

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    name=models.CharField(max_length=50)
+    roll_no=models.IntegerField()
+    group_no=models.CharField(max_length=50)
+
+
+class Teacher(models.Model):
+    name=models.CharField(max_length=50)
+    tid=models.IntegerField()
+    group_no=models.CharField(max_length=50)
+    isCood=models.BooleanField()
