6a2ab5e6a5365cf02daf93c15fb75179f44dfa81
Anuj_Saini <anuj2196@gmail.com>
2018-11-17 19:10:38 +0530

    Add new users in database

--- tracker/models.py
+++ tracker/models.py
@@ -7,9 +7,14 @@ class Student(models.Model):
     roll_no=models.IntegerField()
     group_no=models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.name+'-'+self.group_no
+
 
 class Teacher(models.Model):
     name=models.CharField(max_length=50)
     tid=models.IntegerField()
     group_no=models.CharField(max_length=50)
     isCood=models.BooleanField()
+    def __str__(self):
+        return self.name+'-'+self.group_no
