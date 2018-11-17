969369b7b446b5048969ccc020aef619ef419079
Anuj_Saini <anuj2196@gmail.com>
2018-11-18 01:48:52 +0530

    Add view for teacher and student list

--- tracker/urls.py
+++ tracker/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns=[
     path('', views.index, name='index'),
+    path('students',views.student_list,name='student_list'),
+    path('teachers',views.teacher_list,name='teacher_list')
 ]
--- tracker/views.py
+++ tracker/views.py
@@ -1,6 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Student, Teacher
 # Create your views here.
 
 def index(request):
-    return HttpResponse("<h1>Hello!")
+
+    return render(request,'tracker/layout.html')
+
+
+def student_list(request):
+
+    students=Student.objects.all()
+
+    context={
+        'students':students,
+        'title':'Student List'
+    }
+    return render(request,'tracker/student_list.html',context)
+
+def teacher_list(request):
+
+    teachers=Teacher.objects.all()
+
+    context={
+        'teachers':teachers,
+        'title':'Instructor List'
+    }
+    return render(request,'tracker/teacher_list.html',context)
