206242d5509379cb2f6ba0043b61d16d21cd91c1
Anuj_Saini <anuj2196@gmail.com>
2018-11-19 19:51:14 +0530

    Add links to pages

--- draft/models.py
+++ draft/models.py
@@ -3,8 +3,9 @@ from django.db import models
 # Create your models here.
 class Syllabus(models.Model):
     sno=models.IntegerField()
-    topic=models.CharField(max_length=100)
+    topic=models.TextField()
     lecture_no=models.CharField(max_length=50)
-
+    class Meta:
+        verbose_name_plural="Syllabus"
     def __str__(self):
         return self.topic
